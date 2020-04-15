219757528b0b82ed17eb2f2d468c0223c95a1981
ram5353 <35377735+ram5353@users.noreply.github.com>
2020-04-15 12:12:57 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Smart-Assist';
+  title = 'VWITS-R360';
   description = 'Uploding and Downloading Files';
 }
